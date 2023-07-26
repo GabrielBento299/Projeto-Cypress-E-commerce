81fc5e1514b442370850f1596ad8742f038a6dff
Gabriel Bento <gabrielbento299@hotmail.com>
2023-07-26 11:12:56 -0300

    finished product test

--- cypress/e2e/product.cy.js
+++ cypress/e2e/product.cy.js
@@ -1,40 +1,52 @@
 /// <reference types="cypress" />
 
-describe('Product page tests and Cart', () => {
-    it('should add in cart one product with success', () => {
-       cy.productInCart();
-       cy.get('.shopping_cart > a').click();
-
-        cy.get('#summary_products_quantity')
-            .should('contain','1 product');
-
-        cy.get('tbody tr')
-            .should('be.visible')
-            .and('have.length', 1);
+describe('Product Page and Cart Tests', () => {
+    context('Adding Products to Cart', () => {
+        it('should add in cart one product with success', () => {
+            cy.productInCart();
+            cy.get('.shopping_cart > a').click();
+     
+             cy.get('#summary_products_quantity')
+                 .should('contain','1 product');
+     
+             cy.get('tbody tr')
+                 .should('be.visible')
+                 .and('have.length', 1);
+         });
+         it('should add in cart all product with success', () => {
+             cy.allProductsInCart();
+     
+             cy.get('.shopping_cart > a').click();
+     
+             cy.get('#summary_products_quantity').should('contain','7 product');
+     
+             cy.get('tbody tr')
+                 .should('be.visible')
+                 .and('have.length', 7);
+         });
     });
-    it('should add in cart all product with success', () => {
-        cy.allProductsInCart();
 
-        cy.get('.shopping_cart > a').click();
-
-        cy.get('#summary_products_quantity')
-            .should('contain','7 product');
-
-        cy.get('tbody tr')
-            .should('be.visible')
-            .and('have.length', 7);
-    });
-
-    context('Cart', () => {
+    context('Cart Functionality', () => {
         beforeEach(() => {
             cy.productInCart();
             cy.visit('http://www.automationpractice.pl/index.php?controller=order');
         });
+        
         it('should increase the amount of product when increasing', () => {
             cy.get('#cart_quantity_up_1_1_0_0 > span').click();
             cy.reload();
 
             cy.get('.cart_quantity input[type="text"]').should('have.value', 2);
+        });
+
+        it('should decrease the amount of product when decreasing', () => {
+            cy.get('#cart_quantity_up_1_1_0_0 > span').click();
+            cy.reload();
+            cy.get('.cart_quantity input[type="text"]').should('have.value', 2);
+            
+            cy.get('#cart_quantity_down_1_1_0_0 > span').click();
+            cy.reload();
+            cy.get('.cart_quantity input[type="text"]').should('have.value', 1);
         });
 
         it('should remove product in cart with success', () => {
@@ -52,6 +64,19 @@ describe('Product page tests and Cart', () => {
             cy.reload();
 
             cy.get('#total_price').should('contain', '$41');
+        });
+
+        it('should decrease the o product values by decreasing the quantity', () => {
+            cy.get('#total_price').should('contain', '$24');
+
+            cy.get('#cart_quantity_up_1_1_0_0 > span').click();
+            cy.reload();
+            cy.get('#total_price').should('contain', '$41');
+
+            cy.get('#cart_quantity_down_1_1_0_0 > span').click();
+            cy.reload();
+
+            cy.get('#total_price').should('contain', '$24');
         });
     });
 });
